5c0173b70e4a72d57f76e8bf22828554ecd0d047
MarcoArraiz <marcoarraiz@gmail.com>
2023-03-15 12:56:49 -0300

    objetos a arrays
    cambio del listado de productos de una lista de objetos a un Arrays de objetos

--- main.js
+++ main.js
@@ -1,54 +1,81 @@
-class Producto {
-    constructor(nombre, marca, modelo, id, precio) {
-        this.nombre = nombre
-        this.marca = marca
-        this.modelo = modelo
-        this.id = id
-        this.precio = precio
+const productos = [
+    {
+        nombre: 'Ryzen 5 5600x',
+        descripcion: 'Procesador AMD Ryzen 5 5600X',
+        frecuencia: '3700 - 4600 MHz',
+        nucleos: '6 nucleos - 12 hilos',
+        socket: 'AM4',
+        cache: '6 x 512KB L2 / 32MB L3',
+        precio: 194,
+        //imagen: 'ruta/a/la/imagen1.jpg',
+    },
+    {
+        nombre: 'Ryzen 5 7600',
+        descripcion: 'Procesador AMD Ryzen 5 7600',
+        frecuencia: '3800 MHz',
+        nucleos: '6 nucleos - 12 hilos',
+        socket: 'AM5',
+        cache: '6 x 1MB L2 / 32MB L3',
+        precio: 239,
+        //imagen: 'ruta/a/la/imagen2.jpg',
+    },
+    {
+        nombre: 'Ryzen 9 5900x',
+        descripcion: 'Procesador AMD Ryzen 9 5900X',
+        frecuencia: '3700 MHz',
+        nucleos: '12 núcleos / 24 hilos',
+        socket: 'AM4',
+        cache: '12 x 512KB L2 / 64MB L3',
+        precio: 420,
+        //imagen: 'ruta/a/la/imagen3.jpg',
+    },
+    {
+        nombre: 'Ryzen 9 3900xt',
+        descripcion: 'Procesador AMD Ryzen 9 3900xt',
+        frecuencia: '3700 MHz',
+        nucleos: '12 núcleos / 24 hilos',
+        socket: 'AM4',
+        cache: '12 x 512KB L2 / 64MB L3',
+        precio: 400,
+        //imagen: 'ruta/a/la/imagen4.jpg',
     }
-};
+];
 
-const procesador = new Producto("procesador", "amd", "ryzen", 1, 300);
-const tarjetaGrafica = new Producto("tarjeta grafica", "asus", "rog strix", 2, 900);
-const fuenteAlimentacion = new Producto("fuente de alimentacion", "evga", "nova", 3, 280);
-const placaMadre = new Producto("placa madre", "asus", "tuf", 4, 400);
+// Asignar cada objeto del array 'productos' a una variable
+const procesadorRyzen5600x = productos[1];
+const procesadorRyzen7600 = productos[2];
+const procesadorRyzen5900x = productos[3];
+const procesadorRyzen3900xt = productos[4];
 
-let productoCarrito = parseInt(
-    prompt("Escoge el numero del producto deseado. 1.Procesador - 2.Tarjeta Grafica - 3.Fuente de Alimentaciom - 4.Placa Madre")
-);
-
-let escogioProducto = false
-
-let infoProductoCarrito
+let productoCarrito = parseInt(prompt("Escoge el número del producto deseado. 1.Ryzen 5 5600x - 2.Ryzen 5 7600 - 3.Ryzen 9 5900x - 4.Ryzen 9 3900xt"));
+let escogioProducto = false;
+let infoProductoCarrito;
 
 while (escogioProducto === false) {
     if (productoCarrito === 1) {
-        escogioProducto = true
-        infoProductoCarrito = procesador
+        escogioProducto = true;
+        infoProductoCarrito = procesadorRyzen5600x;
     } else if (productoCarrito === 2) {
-        escogioProducto = true
-        infoProductoCarrito = tarjetaGrafica
+        escogioProducto = true;
+        infoProductoCarrito = procesadorRyzen7600;
     } else if (productoCarrito === 3) {
-        escogioProducto = true
-        infoProductoCarrito = fuenteAlimentacion
+        escogioProducto = true;
+        infoProductoCarrito = procesadorRyzen5900x;
     } else if (productoCarrito === 4) {
-        escogioProducto = true
-        infoProductoCarrito = placaMadre
+        escogioProducto = true;
+        infoProductoCarrito = procesadorRyzen3900xt;
     } else {
-        productoCarrito = parseInt(
-            prompt(
-                "Escoge UN numero de producto CORRECTO. 1.Procesador - 2.Tarjeta Grafica - 3.Fuente de Alimentaciom - 4.Placa Madre"
-            )
-        )
+        productoCarrito = parseInt(prompt("Escoge un número de producto correcto. 1.Ryzen 5 5600x - 2.Ryzen 5 7600 - 3.Ryzen 9 5900x - 4.Ryzen 9 3900xt"));
     }
 };
+
 alert("El producto escogido es: " + infoProductoCarrito.nombre + " se ha agregado al carrito");
 console.log("infoCarrito", infoProductoCarrito);
 
 function calcularPrecio(producto) {
-  return producto.precio;
+    return producto.precio;
 }
 
 let precioAPagar = calcularPrecio(infoProductoCarrito);
 alert("El precio a pagar es: " + precioAPagar)
-console.log("Precio del producto: " + precioAPagar); // Agregar esta línea
+console.log("Precio del producto: " + precioAPagar);
